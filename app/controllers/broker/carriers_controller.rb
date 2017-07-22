320b85afee95b5d9ab0c19ae13c64ee41df376e6
Joshua Reid <reid.joshuathomas@gmail.com>
2017-07-22 18:56:04 -0500

    yes

--- app/controllers/broker/carriers_controller.rb
+++ app/controllers/broker/carriers_controller.rb
@@ -5,6 +5,11 @@ class Broker::CarriersController < ApplicationController
 
   def show
     @carrier = Carrier.find(params[:id])
+    @loads = []
+    @loads << Load.find(1)
+    @loads << Load.find(2)
+    @loads << Load.find(3)
+    @loads << Load.find(4)
+    @loads << Load.find(5)
   end
-
 end
