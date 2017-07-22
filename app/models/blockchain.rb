92945656bfc098fc5e6f77c8d1429fe71bce0108
Juan Hinojo <juan@cratebind.com>
2017-07-22 16:53:10 -0500

    added print for seed data

--- app/models/blockchain.rb
+++ app/models/blockchain.rb
@@ -2,6 +2,7 @@ class Blockchain
 
   def self.sync
     [Broker, Carrier, Shipper, Load, LoadCancellation, LoadPickup, LoadDelivery].each do |klass|
+      print '.'
       klass.sync
     end
   end
