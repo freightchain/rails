2e97fd9595e83437aeb62d90db0870ae3ae482a0
Jordan Graft <jordan@cratebind.com>
2017-07-22 15:07:40 -0500

    Correct the primary keys

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -10,11 +10,11 @@ class Broker
 
   def self.seed_data
     [
-      {"$class" =>  chain_klass, id: '1', name: 'XPO Logistics', mc_number: '1234'},
-      {"$class" =>  chain_klass, id: '2', name: 'CHR Robinson', mc_number: '1235'},
-      {"$class" =>  chain_klass, id: '3', name: 'TQL Logistics', mc_number: '1236'},
-      {"$class" =>  chain_klass, id: '4', name: 'Interstate Logistics', mc_number: '1237'},
-      {"$class" =>  chain_klass, id: '5', name: 'Coyote Logistics', mc_number: '1238'},
+      {"$class" =>  chain_klass, brokerId: '1', name: 'XPO Logistics', mc_number: '1234'},
+      {"$class" =>  chain_klass, brokerId: '2', name: 'CHR Robinson', mc_number: '1235'},
+      {"$class" =>  chain_klass, brokerId: '3', name: 'TQL Logistics', mc_number: '1236'},
+      {"$class" =>  chain_klass, brokerId: '4', name: 'Interstate Logistics', mc_number: '1237'},
+      {"$class" =>  chain_klass, brokerId: '5', name: 'Coyote Logistics', mc_number: '1238'},
     ]
   end
 
--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -10,13 +10,13 @@ class Carrier
 
   def self.seed_data
     [
-      {"$class" =>  chain_klass, id: '1', name: 'Swift Logistics', mc_number: '1234'},
-      {"$class" =>  chain_klass, id: '2', name: 'EHR Trucking', mc_number: '1235'},
-      {"$class" =>  chain_klass, id: '3', name: 'Texas Van Lines', mc_number: '1236'},
-      {"$class" =>  chain_klass, id: '4', name: 'Knight Transportation', mc_number: '1237'},
-      {"$class" =>  chain_klass, id: '5', name: 'Atlas Van Lines', mc_number: '1238'},
-      {"$class" =>  chain_klass, id: '6', name: 'Fraudulent Carrier', mc_number: '1238'},
-      {"$class" =>  chain_klass, id: '7', name: 'Really Good Carrier', mc_number: '1238'},
+      {"$class" =>  chain_klass, carrierId: '1', name: 'Swift Logistics', mc_number: '1234'},
+      {"$class" =>  chain_klass, carrierId: '2', name: 'EHR Trucking', mc_number: '1235'},
+      {"$class" =>  chain_klass, carrierId: '3', name: 'Texas Van Lines', mc_number: '1236'},
+      {"$class" =>  chain_klass, carrierId: '4', name: 'Knight Transportation', mc_number: '1237'},
+      {"$class" =>  chain_klass, carrierId: '5', name: 'Atlas Van Lines', mc_number: '1238'},
+      {"$class" =>  chain_klass, carrierId: '6', name: 'Fraudulent Carrier', mc_number: '1238'},
+      {"$class" =>  chain_klass, carrierId: '7', name: 'Really Good Carrier', mc_number: '1238'},
     ]
   end
 
