ec83a772f850f025d718b3224cdaa1276e85b9e8
Joshua Reid <reid.joshuathomas@gmail.com>
2017-07-22 14:13:40 -0500

    still initing

---
+++ app/controllers/broker/carriers_controller.rb
@@ -0,0 +1,7 @@
+class Broker::CarriersController < ApplicationController
+
+  def index
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root to: "broker/carriers#index"
 
-  root to: "statics#index"
+  
+  namespace :broker do 
+    resources :carriers    
 
+  end
 end
